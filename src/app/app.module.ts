ddbcb3137555ca5f31af7e8053616a104b874a21
bhushanjss <bhushanjss@gmail.com>
2018-06-25 22:53:20 -0700

    init various components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import  BeerDetailsComponent  from './components/beer-details/beer-details.component';
+import  BeerSearchComponent  from './components/beer-search/beer-search.component';
+import  BeerListComponent  from './components/beer-list/beer-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerDetailsComponent,
+    BeerSearchComponent,
+    BeerListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/beer-search/beer-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beer-search',
+  templateUrl: './beer-search.component.html',
+  styleUrls: ['./beer-search.component.css']
+})
+export default class BeerSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
