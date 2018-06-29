ba9d0a662c23fd9cff144916764253b0ec4f5e17
bhushanjss <bhushanjss@gmail.com>
2018-06-29 01:14:20 -0700

    fixed the proxy server API issue, updated readme.

--- src/app/services/brewery.service.ts
+++ src/app/services/brewery.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const httpOptions = {
-  headers: new HttpHeaders().set('Access-Control-Allow-Origin', '*')
-}
-
 @Injectable()
 export class BreweryService {	
   
   beerLists = [];
 
-  apiURL = "http://api.brewerydb.com/v2";
   apiKey = "bf400561b993d3a1e281916f00df36c7";
 
   constructor(private http: HttpClient) { }
 
   getRandomBrew(): any {
-  	return this.http.get(`${this.apiURL}/beers?key=${this.apiKey}&abv=5&withBreweries=Y`, httpOptions);
+  	return this.http.get(`/v2/beers?key=${this.apiKey}&abv=5&withBreweries=Y`);
   }
 
   searchBeers(query: string, type: string): any {
-    const url = `${this.apiURL}/search?key=${this.apiKey}&q=${query}&type=${type}`;
-    return this.http.get(url, httpOptions);
+    const url = `/v2/search?key=${this.apiKey}&q=${query}&type=${type}`;
+    return this.http.get(url);
   }
 }
